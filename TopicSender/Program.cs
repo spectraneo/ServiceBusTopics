70ca28ba185180730faba3c21a51518ca6e02a70
Nicolas Anderson <nicolas-31@outlook.com>
2025-08-10 15:09:02 +0200

    updated

--- TopicSender/Program.cs
+++ TopicSender/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Azure.Identity;
+
+// client to create senders and receivers 
+ServiceBusClient client;
+
+// sender to publish messsages to a topic 
+ServiceBusSender sender;
+
+// number of messages to be sent to the topic 
+const int numOfMessages = 3;
+
+// creat a client to connect to our Service Bus namespace
+client = new ServiceBusClient(
+    "nicolasnamespace.servicebus.windows.net",
+    new DefaultAzureCredential());
+
+sender = client.CreateSender("mytopic");
+
+// create a batch
+using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+for (int i = 1; i <= numOfMessages; i++)
+{
+    // try adding a message to the batch 
+    if(!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+    {
+        // if the message is too large to fit in the batch, throw an exception
+        throw new Exception($"Message {i} is too large to fit in the batch.");
+    }
+}
+
+try
+{
+    // send the batch of messages to the topic 
+    await sender.SendMessagesAsync(messageBatch);
+    Console.WriteLine($"A batch of {numOfMessages} messages have been published to the topic. ");
+}
+finally 
+{
+    //close the sender 
+    await sender.DisposeAsync();
+
+    // close the client 
+    await client.DisposeAsync();
+}
+
+
+Console.WriteLine("Press any key to end the application");
+Console.ReadKey();
